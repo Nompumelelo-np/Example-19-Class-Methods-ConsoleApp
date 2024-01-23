baa065837dea24cd3cb602b8189075d88113a242
Acer-User <Acer-User@DESKTOP-NSD4PLV>
2024-01-23 20:59:38 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_19_Class_Methods_ConsoleApp
+{
+
+    internal class  Student
+    {
+        public double weight;
+        public string name;
+        public int age;
+
+        public Student(double aWeight, string aName, int aAge)
+        {
+          this .weight = aWeight;
+          this .name = aName;
+          this .age = aAge;
+        } 
+
+        public bool IsObese()
+        {
+            if (weight > 60)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+        static void Main(string[] args)
+        {
+            Student student1 = new Student(70, "John", 15);
+            Student student2 = new Student(40, "Tim", 10);
+
+            Console.WriteLine(student1.IsObese() );
+            Console.WriteLine(student2.IsObese());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Example 19-Class Methods ConsoleApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Example 19-Class Methods ConsoleApp")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("20227074-f1ce-4831-923e-2413fbf1a3a5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
